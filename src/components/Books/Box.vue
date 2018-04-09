faeb9702630ba45cecfc5342c98f10e6aaf5cc50
Brendan Thomas <bt331609@gmail.com>
2018-04-08 21:50:16 -0400

    list books for authors

---
+++ src/components/Books/Box.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="col book text-center">
+    <img :src="image"></img>
+    <p>{{ book.title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Box',
+  props: ['book'],
+  data() {
+    return {
+      image:
+      `http://via.placeholder.com/200x200?text=${encodeURIComponent(this.book.title)}`
+    }
+  },
+  created() {
+    this.pullImage();
+  },
+  methods: {
+    async pullImage() {
+      const response = await
+        fetch(`http://api.duckduckgo.com/?q=${encodeURIComponent(this.book.title)}&format=json&pretty=1`);
+      const json = await response.json();
+      if (json.Image) {
+        this.image = json.Image;
+      };
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.book {
+  min-width: 260px;
+
+  img {
+    object-fit: cover;
+    width: 100%;
+    height: 260px;
+  }
+
+  a {
+    color: #23ce6b;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import Login from '@/components/Login';
 import Logout from '@/components/Logout';
 import AuthorsList from '@/components/Authors/List'
+import SingleAuthor from '@/components/Authors/Single'
 
 Vue.use(Router);
 
@@ -22,6 +23,11 @@ export default new Router({
       path: '/authors',
       name: 'Authors',
       component: AuthorsList
+    },
+    {
+      path: '/authors/:id',
+      name: 'Author',
+      component: SingleAuthor
     }
   ],
 });
